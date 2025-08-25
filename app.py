66a01624645380fca842a535bacf7df28c0b455d
lukaszpancer <pancer.lukasz@gmail.com>
2025-08-25 21:38:27 +0200

    fix: back to autotokenizer

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from transformers import pipeline, Conversation, LlamaForCausalLM, LlamaTokenizer
+from transformers import pipeline, Conversation, LlamaForCausalLM, AutoTokenizer
 
 
 st.markdown("# Hi! I'm a chatbot")
@@ -14,7 +14,7 @@ def get_chatbot():
     model = LlamaForCausalLM.from_pretrained(
         "unsloth/tinyllama-chat-bnb-4bit", ignore_mismatched_sizes=True
     )
-    tokenizer = LlamaTokenizer.from_pretrained("unsloth/tinyllama-chat-bnb-4bit")
+    tokenizer = AutoTokenizer.from_pretrained("unsloth/tinyllama-chat-bnb-4bit")
     return pipeline(task="conversational", model=model, tokenizer=tokenizer)
 
 
